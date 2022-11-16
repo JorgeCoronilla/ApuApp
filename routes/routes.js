326beac0c3014b5d4e13afec0fc0a5cae1d3ad44
JorgeCoronilla <113173137+JorgeCoronilla@users.noreply.github.com>
2022-11-16 17:22:45 +0100

    Add files via upload

---
+++ routes/routes.js
@@ -0,0 +1,41 @@
+//module.exports = router;
+
+/*
+/
+clicks
+
+/login
+{user,password}
+
+/login/recover
+{user,password}
+
+/register
+{name, surname,email,address,bankAcount,pass,pass2}
+
+
+/session/:user/
+/session/:user/dash
+{name, surname,email,address,bankAcount,pass,pass2}
+
+/session/:user/shop
+click
+
+/session/:user/cart
+{products, quantity}
+
+/session/:user/payment
+{user, bankAcount, atc}
+
+/session/:user/payment/success
+
+
+/session/:admin/dash
+{name, surname,email,address,bankAcount,pass,pass2}
+
+
+/repairs/
+/repairs/:tienda
+{contact-details}
+
+*/
