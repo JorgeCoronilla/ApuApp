3aa4568d32620265b43353968a591a794b163685
SERGIOOVEP <sergio.ovejeropascual@gmail.com>
2022-11-21 14:15:14 +0100

    añadido mapas

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -5,49 +5,63 @@ const { json } = require("express");
 const Register = {
 
     start: async (req, res) => {
-        res.render("../views/userRegister.ejs",{message:""});
+        res.render("../views/userRegister.ejs", { message: "" });
     },
 
     //VOLVEMOS A LA PANTALLA DE INICIO UNA VEZ REGISTRADO
     index: async (req, res) => {
-        res.render("../views/index.ejs");
+        res.render("../views/login.ejs");
+
     },
 
     //INSERTAMOS USUARIOS
     insertUser: async (req, res) => {
         //await getConnection()
         const { user_name, surname_1, surname_2, address, email, user_pass } = req.body
-        try{
+        try {
             const userId = await addUser(user_name, surname_1, surname_2, address, email, user_pass)
-            if(userId){
-                res.render("registerConfirmed",{ user_name });
-                //res.render("../views/userRegister.ejs");
+            if (userId) {
+                //res.render("registerConfirmed",{ user_name });
+                res.render("../views/map.ejs");
             }
-        }catch(error){
-            if(error == "ER_DUP_ENTRY"){ //ES EL MENSAJE QUE NOS DA LA CONSOLA CUANDO EL REGISTRO DEL CORREO ESTÁ DUPLICADO EN LA BASE DE DATOS.
-                res.render("userRegister",{message:"El correo introducido ya existe.Por favor introduzca uno válido"});
-                
-               // res.status(400).json({ message: "El correo electrónico introducido ya existe.Por favor introduzca uno válido" })
+        } catch (error) {
+            if (user_name == "" || surname_1 == "" || surname_2 == "" || address == "" || email == "" || user_pass == "") {
+                res.render("userRegister", { message: "Todos los campos son obligatorios" });
+
+            } else if (error == "ER_DUP_ENTRY") { //ES EL MENSAJE QUE NOS DA LA CONSOLA CUANDO EL REGISTRO DEL CORREO ESTÁ DUPLICADO EN LA BASE DE DATOS.
+                res.render("userRegister", { message: "El correo introducido ya existe.Por favor introduzca uno válido" });
+
+                // res.status(400).json({ message: "El correo electrónico introducido ya existe.Por favor introduzca uno válido" })
             }
         }
     },
 
-    //CONSULTAMOS TODOS LOS DATOS DE LA TABLA USUARIOS
-    findAll: async (req, res) => {
-        const connection = await getConnection()
-        connection.query('SELECT * FROM users', function (err, filas) {
-            if (err) {
-                throw err;
-            } else {
-                filas.forEach(fila => {
-                    console.log(fila)
-                });
-            }
-        })
+
+
+    map: async (req, res) => {
+        res.render("map");
 
     }
 
+
+
+
+    /*
+        //CONSULTAMOS TODOS LOS DATOS DE LA TABLA USUARIOS
+        findAll: async (req, res) => {
+            const connection = await getConnection()
+            connection.query('SELECT * FROM users', function (err, filas) {
+                if (err) {
+                    throw err;
+                } else {
+                    filas.forEach(fila => {
+                        console.log(fila)
+                    });
+                }
+            })
     
+        }
+    */
 }
 
 
@@ -55,28 +69,28 @@ const Register = {
 
 //CREAMOS UNA FUNCIÓN PARA ENCRIPTAR LA CONTRASEÑA.
 const encryptUserPass = async (textPlain) => {
-    const hash = await bcrypt.hash(textPlain,10)
+    const hash = await bcrypt.hash(textPlain, 10)
     return hash
 }
 
 
 //CREAMOS FUNCION PARA AÑADIR USUARIO.
 const addUser = async (user_name, surname_1, surname_2, address, email, user_pass) => {
-    
-        const connection = await getConnection()
-        const passwordEncrypted = await encryptUserPass(user_pass)
-        const datesUser = { user_name, surname_1, surname_2, address, email, user_pass:passwordEncrypted }
-        //HACEMOS PROMESA "MANUAL".SI LO INSERTA SALE POR RESOLVE Y LO ENVÍA A "INSERTEUSER" ,Y SI FALLA SALE POR REJECT.
-        return new Promise((resolve,reject)=>{
-            connection.query("INSERT INTO users SET ?", datesUser ,(error,results) => {
-                if (error){
-                    return reject(error.code)
-                }
-                const response = JSON.parse(JSON.stringify(results))
-                resolve(response.insertId)
-            })
+
+    const connection = await getConnection()
+    const passwordEncrypted = await encryptUserPass(user_pass)
+    const datesUser = { user_name, surname_1, surname_2, address, email, user_pass: passwordEncrypted }
+    //HACEMOS PROMESA "MANUAL".SI LO INSERTA SALE POR RESOLVE Y LO ENVÍA A "INSERTEUSER" ,Y SI FALLA SALE POR REJECT.
+    return new Promise((resolve, reject) => {
+        connection.query("INSERT INTO users SET ?", datesUser, (error, results) => {
+            if (error) {
+                return reject(error.code)
+            }
+            const response = JSON.parse(JSON.stringify(results))
+            resolve(response.insertId)
         })
-   
+    })
+
 }
 
 
--- routes/register.js
+++ routes/register.js
@@ -4,6 +4,7 @@ const Register = require("../controllers/register.controller")
 register.get('/', Register.start);
 register.post("/", Register.insertUser)
 register.get("/confirmed", Register.index)
+register.get("/map", Register.map)
 
 //register.get('/findall',Register.findAll)
 
